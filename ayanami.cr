e81246eae935c7f938a5cf1ece8f67f9360f865d
Wendel Scardua <wendelscardua@gmail.com>
2020-12-24 22:23:41 -0300

    Move definitions to yaml

--- ayanami.cr
+++ ayanami.cr
@@ -1,5 +1,6 @@
 require "stumpy_png"
 require "crystaledge"
+require "yaml"
 require "./v3-helpers"
 require "./camera"
 require "./ray"
@@ -31,7 +32,7 @@ class Ayanami
           v = (j + rand) / (height - 1)
           ray = camera.ray(u, v)
           ray_color(ray, world, max_depth)
-        end.sum(V3.new(0.0, 0.0, 0.0)) / samples_per_pixel.to_f
+        end.sum / samples_per_pixel.to_f
 
         canvas[i, height - j - 1] = StumpyPNG::RGBA.from_rgb_n((Math.sqrt(color.x) * 255.99999).to_i,
                                                                (Math.sqrt(color.y) * 255.99999).to_i,
@@ -63,26 +64,60 @@ class Ayanami
   end
 end
 
-width = 640
-height = 480
+config = YAML.parse(File.read(ARGV[0]))
 
-camera = Camera.new(vertical_fov: 90.0, aspect_ratio: width.to_f / height)
+width = config["options"]["width"].as_i
+height = config["options"]["height"].as_i
 
-mat_ground = Lambertian.new(V3.new(0.8, 0.8, 0.0))
-mat_solid = Lambertian.new(V3.new(0.1, 0.2, 0.5))
-mat_glass = Dielectric.new(1.7)
-mat_metal = Metal.new(V3.new(0.8, 0.5, 0.1), 0.0)
+camera = Camera.new(vertical_fov: config["camera"]["vertical_fov"].as_f,
+                    aspect_ratio: width.to_f / height)
+
+materials = Hash(String, Material).new
+
+config["materials"].as_h.each do |name, args|
+  material_type = args["type"].as_s
+  materials[name.as_s] = case material_type
+                         when "lambertian"
+                           albedo = V3.new(args["albedo"][0].as_f,
+                                           args["albedo"][1].as_f,
+                                           args["albedo"][2].as_f)
+                           Lambertian.new(albedo)
+                         when "dielectric"
+                           refraction_index = args["refraction_index"].as_f
+                           Dielectric.new(refraction_index)
+                         when "metal"
+                           albedo = V3.new(args["albedo"][0].as_f,
+                                           args["albedo"][1].as_f,
+                                           args["albedo"][2].as_f)
+                           fuzz = args["fuzz"].as_f
+                           Metal.new(albedo, fuzz)
+                         else
+                           raise "Invalid material type #{material_type}"
+                         end
+end
 
 world = HittableList.new
-world << Sphere.new(V3.new(0.0, -100.5, -1.0), 100.0, mat_ground)
-world << Sphere.new(V3.new(0.0, 0.0, -1.5), 0.5, mat_solid)
-world << Sphere.new(V3.new(-1.0, 0.0, -1.0), 0.5, mat_glass)
-world << Sphere.new(V3.new(1.0, 0.0, -1.0), 0.5, mat_metal)
+
+config["world"].as_a.each do |object|
+  object_type = object["type"].as_s
+  object = case object_type
+           when "sphere"
+             center = V3.new(object["center"][0].as_f,
+                             object["center"][1].as_f,
+                             object["center"][2].as_f)
+             radius = object["radius"].as_f
+             material = object["material"].as_s
+             Sphere.new(center, radius, materials[material])
+           else
+             raise "Invalid object type #{object_type}"
+           end
+  world << object
+end
 
 ayanami = Ayanami.new width: width, height: height,
-                      samples_per_pixel: 64,
-                      max_depth: 32,
+                      samples_per_pixel: config["options"]["samples_per_pixel"].as_i,
+                      max_depth: config["options"]["max_depth"].as_i,
                       world: world,
                       camera: camera
 
-ayanami.run(output: "ayanami.png")
+ayanami.run(output: ARGV[1])
